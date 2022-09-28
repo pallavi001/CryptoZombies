86aa8c7851ccfdb5299f4403eaa99972c6b1ce34
Pallavi <pallavisuri001@gmail.com>
2022-09-28 13:16:01 +0530

    Lesson1 Chapter5 completed

--- Code/Contract.sol
+++ Code/Contract.sol
@@ -1,4 +1,14 @@
 pragma solidity >=0.5.0 <0.9.0;
 
-contract ZombiesContract {
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
 }
+
